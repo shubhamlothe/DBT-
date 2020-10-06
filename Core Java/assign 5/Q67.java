13a99c673fae0e98f7ac84de03969dccdf9e9077
shubhamlothe <58683237+shubhamlothe@users.noreply.github.com>
2020-10-06 21:48:44 +0530

    Add files via upload

---
+++ Core Java/assign 5/Q67.java
@@ -0,0 +1,20 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class Ques67 {
+
+	public static void main(String[] args) {
+		try {
+			FileInputStream is = new FileInputStream("D://CDAC//JAVA//CDAC//Java//TextSol//Assign1.txt");
+			ObjectInputStream ois = new ObjectInputStream(is);
+			Ques67 emp = (Ques67) ois.readObject();
+			ois.close();
+			is.close();
+			System.out.println(emp.toString());
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
